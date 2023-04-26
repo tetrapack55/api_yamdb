5231db0a06f9913bd9244bfb395ab8a3956cab15
Natali7077 <120510582+Natali7077@users.noreply.github.com>
2023-04-26 22:23:05 +0300

    Добавлена модель категорий

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(
+        verbose_name='Название категории',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.slug
